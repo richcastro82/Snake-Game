c43eaf5cf4a2deb21cde4d1d6a5ae06b0a69979a
richcastro82 <rich.castro@achesongroup.com>
2021-11-21 08:54:31 -0500

    Fruit Class & Graphics

--- snake.py
+++ snake.py
@@ -5,18 +5,46 @@
 # and now I am just creating a few games with what I learned
 # before I literally forget it all.
 
-import pygame, sys, os
-w=800
-h=800
-s=(w,h)
+import pygame, sys, os, random
+from pygame.math import Vector2
+cell_size=40
+cell_number=20
 fps=30
 clock=pygame.time.Clock()
+
+
+class FRUIT:
+    def __init__(self):
+        self.x=random.randint(0,cell_number-1)
+        self.y=random.randint(0,cell_number-1)
+        self.pos=Vector2(self.x, self.y)
+
+    def draw_fruit(self):
+        fruit_rect=pygame.Rect(self.pos.x*cell_size, self.pos.y*cell_size,cell_size,cell_size)
+        pygame.draw.rect(gameScreen,(0,0,0), fruit_rect)
+
+
+
+
+
 pygame.init()
-gameScreen=pygame.display.set_mode(s)
+gameScreen=pygame.display.set_mode((cell_number*cell_size, cell_number*cell_size))
+
+
+fruit=FRUIT()
+# snake=SNAKE()
+# main game loop
+
+screenUpdate=pygame.USEREVENT
+pygame.time.set_timer(screenUpdate, 150)
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
+        # if event.type==screenUpdate:
+        #     snake.move_snake()
     gameScreen.fill((120,120,120))
+    fruit.draw_fruit()
+    # snake.draw_snake()
     pygame.display.update()
     clock.tick(fps)
